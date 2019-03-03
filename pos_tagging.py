4a7bd25afd4299724714ca963a4b4bc2526aa809
fvera02 <fvera@stanford.edu>
2019-03-03 14:56:08 -0800

    Added library download instructions and created data folder

--- pos_tagging.py
+++ pos_tagging.py
@@ -1,4 +1,5 @@
 import nltk
+from operator import itemgetter
 
 def txt_to_string(filename):
 	file = open(filename, "r")
@@ -7,22 +8,19 @@ def txt_to_string(filename):
 
 # txt_to_string("ladygaga.txt")
 
-tokens = nltk.word_tokenize(txt_to_string("ladygaga.txt"))
+tokens = nltk.word_tokenize(txt_to_string("data/ladygaga.txt"))
 # print tokens
 
 pos_labels = nltk.pos_tag(tokens)
 # print pos_labels
 
-# all_adjectives = []
-
 adjective_counts = {}
 
 for pair in pos_labels:
-	if pair[1] == "JJ" or pair[1] == "JJR" or pair[1] == "JJS":
+	if pair[1] == "JJ" or pair[1] == "JJR" or pair[1] == "JJS": #refer to pos_dictionar.txt for meanings of different tags
 		if pair[0] not in adjective_counts.keys():
 			adjective_counts[pair[0]] = 1
 		else:
 			adjective_counts[pair[0]] += 1
-		# all_adjectives.append(pair)
 
-print adjective_counts
+print sorted(adjective_counts.items(), key=itemgetter(1)) #sort by adjective count in ascending order
