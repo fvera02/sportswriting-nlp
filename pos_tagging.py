35e5e53d9fc6a6b717477f66ee8d3a2df834159e
fvera02 <fvera@stanford.edu>
2019-03-05 13:20:06 -0800

    Added .txt files to data folder; Updated pos_tagging.py to return csv of counts

--- pos_tagging.py
+++ pos_tagging.py
@@ -1,26 +1,79 @@
 import nltk
 from operator import itemgetter
+import time
+import csv
+import sys
+reload(sys)
+
+sys.setdefaultencoding("ISO-8859-1")
+
+start_time = time.time()
 
 def txt_to_string(filename):
-	file = open(filename, "r")
+	file = open(filename, "rb")
 	return file.read()
 	# print file.read()
 
-# txt_to_string("ladygaga.txt")
+all_words = ''
 
-tokens = nltk.word_tokenize(txt_to_string("data/ladygaga.txt"))
+### UNCOMMENT below for NBA files
+filenames = txt_to_string("data/NBAfilenames.txt").split()
+for name in filenames:
+	data_name = "data/NBA_txt_Files/" + name
+	all_words += txt_to_string(data_name)
+
+# print all_words.len()
+### UNCOMMENT below for WNBA files
+# all_words += txt_to_string("data/WNBA_Main.txt")
+
+### UNCOMMENT below for WTA files
+# filenames = txt_to_string("data/WTAfilenames.txt").split()
+# for name in filenames:
+# 	data_name = "data/WTA_txt_Files/" + name
+# 	all_words += txt_to_string(data_name)
+
+### UNCOMMENT below for ATP files
+# filenames = txt_to_string("data/ATPfilenames.txt").split()
+# for name in filenames:
+# 	data_name = "data/ATP_txt_Files/" + name
+# 	all_words += txt_to_string(data_name)
+
+tokens = nltk.word_tokenize(all_words)
 # print tokens
+# count = 0
+# for word in tokens:
+# 	count += 1
+# print count
+
+all_word_counts = {}
+
+for word in tokens:
+	if word not in all_word_counts.keys():
+		all_word_counts[word] = 1
+	else:
+		all_word_counts[word] += 1
+
+print sorted(all_word_counts.items(), key = itemgetter(1))
 
 pos_labels = nltk.pos_tag(tokens)
 # print pos_labels
 
-adjective_counts = {}
+descriptive_counts = {}
 
 for pair in pos_labels:
-	if pair[1] == "JJ" or pair[1] == "JJR" or pair[1] == "JJS": #refer to pos_dictionar.txt for meanings of different tags
+	if pair[1] == "JJ" or pair[1] == "JJR" or pair[1] == "JJS" or pair[1] == "RB" or pair[1] == "RBR" or pair[1] == "RBS": #refer to pos_dictionar.txt for meanings of different tags
 		if pair[0] not in adjective_counts.keys():
-			adjective_counts[pair[0]] = 1
+			descriptive_counts[pair[0]] = 1
 		else:
-			adjective_counts[pair[0]] += 1
+			descriptive_counts[pair[0]] += 1
 
-print sorted(adjective_counts.items(), key=itemgetter(1)) #sort by adjective count in ascending order
+print sorted(descriptive_counts.items(), key=itemgetter(1)) #sort by adjective count in ascending order
+
+myFile = open('NBAcounts.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(descriptive_counts.items())
+     
+print("Writing complete")
+
+print("--- %s seconds ---" % (time.time() - start_time))
